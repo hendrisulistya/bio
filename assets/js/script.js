0dd56fc7a448d716c67bed93281943bfc59ef830
Hendri Sulistya <id.hendrisulistya@gmail.com>
2024-09-11 11:06:29 +0700

    layout update nav-toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
+// Function to load content into the #content div and update active link
 function loadContent(section) {
   const content = document.getElementById("content");
+  const links = document.querySelectorAll(".nav-items a");
+
+  // Disable all links
+  links.forEach((link) => link.classList.remove("active"));
+
+  // Enable clicked link
+  const activeLink = document.querySelector(`.nav-items a[href="#${section}"]`);
+  activeLink.classList.add("active");
+
   fetch(`/section/${section}.html`)
     .then((response) => response.text())
     .then((data) => {
@@ -17,3 +27,13 @@ function loadInitialContent() {
 
 // Event listener for window onload to load initial content
 window.addEventListener("load", loadInitialContent);
+
+// Add event listeners to navigation links
+document.querySelectorAll(".nav-items a").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    // Prevent the default action if the link is active
+    if (this.classList.contains("active")) {
+      event.preventDefault();
+    }
+  });
+});
